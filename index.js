79b28d1d8729613ac2c7b4905586e865ac3ad696
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-05 21:55:02 -0500

    Initialize express backend app

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const app = require('./app');
+const appPort = process.env.PORT || 3001;
+
+app.listen(appPort, () => {
+  console.log(`Server running on http://localhost:${appPort}`);
+});
