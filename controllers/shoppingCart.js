42c3ae9d5b856c8f0c71be151f26fb1dc6a79715
Homeroso <82355484+Homeroso@users.noreply.github.com>
2025-02-21 01:06:50 -0500

    Fix getCart query

--- controllers/shoppingCart.js
+++ controllers/shoppingCart.js
@@ -12,31 +12,33 @@ const getShoppingCart = (request, response) => {
     })
   }
 
-  db.query(
-    'SELECT * FROM carrito WHERE idUsuario = ?',
-    [idUsuario],
-    (error, results) => {
-      if (error) {
-        console.error('Error realizando la consulta ', error)
-        return response.status(500).json({
-          success: false,
-          message: 'Error obteniendo carrito',
-          error: error.message,
-        })
-      }
-      return response.status(200).json({
-        success: true,
-        message: 'Carrito obtenido con exito',
-        results,
-      })
-    }
-  ).catch((error) => {
+  try{
+    db.query(
+        'SELECT * FROM vista_productos_carrito_usuario WHERE idUsuario = ?',
+        [idUsuario],
+        (error, results) => {
+          if (error) {
+            console.error('Error realizando la consulta ', error)
+            return response.status(500).json({
+              success: false,
+              message: 'Error obteniendo carrito',
+              error: error.message,
+            })
+          }
+          return response.status(200).json({
+            success: true,
+            message: 'Carrito obtenido con exito',
+            results,
+          })
+        }
+      )
+  } catch{(error) => {
     return response.status(500).json({
       success: false,
       message: 'Error obteniendo carrito',
       error: error.message,
     })
-  })
+  }}
 }
 
 module.exports = { getShoppingCart }
